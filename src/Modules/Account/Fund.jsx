2b742e60113e20298e1d781bad8d23fcfc985a94
gravitonweb-IT <gravitonweb@gmail.com>
2023-10-13 19:48:51 +0530

    lastestchecked

--- src/Modules/Account/Fund.jsx
+++ src/Modules/Account/Fund.jsx
@@ -1,165 +1,159 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+
 import image5 from "../../Assests/FundPageImages/QRcodeheading1.svg";
+
 import image2 from "../../Assests/FundPageImages/accountdetailheading1.svg";
+
 import image4 from "../../Assests/FundPageImages/Qrcodeimg1.svg";
+
 import image3 from "../../Assests/FundPageImages/UPIheading1.svg";
+
 import image7 from "../../Assests/FundPageImages/UPIdetail.svg";
+
 import image8 from "../../Assests/FundPageImages/bankaccount.svg";
+
 import WithdrawForm from "../Account/WithdrawForm";
-import { useNavigate } from "react-router-dom";
 
-
-// const FlipCard = ({ buttonText, image }) => {
-//   const [isFlipped, setIsFlipped] = useState(false);
-//   const [isFormOpen, setIsFormOpen] = useState(false);
-//   const [isFundsPopupOpen, setIsFundsPopupOpen] = useState(false);
-
-//   const handleFlip = () => {
-//     setIsFlipped(!isFlipped);
-//   };
-
-//   const handleButtonClick = () => {
-//     if (buttonText === "Please click button for withdraw") {
-//       setIsFormOpen(true);
-//     } else if (buttonText === "Please click button for funds") {
-//       setIsFundsPopupOpen(true);
-//     }
-//   };
-
-//   const closeForm = () => {
-//     setIsFormOpen(false);
-//   };
-//   const closeFundsPopup = () => {
-//     setIsFundsPopupOpen(false);
-//   };
-
-//   return (
-//     <></>
-//     // <div
-//     //   className="w-72 h-80 bg-white shadow-lg overflow-hidden mx-4 my-8 relative"
-//     //   onMouseEnter={handleFlip}
-//     //   onMouseLeave={handleFlip}
-//     // >
-//     //   <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-//     //     <div key="front">
-//     //       <div className="bg-gray-200 h-full">
-//     //         <img src={image} className="w-[300px] h-[350px] object-cover" alt="Card" />
-//     //       </div>
-//     //     </div>
-//     //     <div key="back" style={{ width: '100%', height: '100%' }}>
-//     //       <div className="bg-[#2774AE] text-center h-full flex flex-col justify-center items-center">
-//     //         <p className="text-2xl text-white font-bold  py-20 ">{buttonText}</p>
-//     //         <button
-//     //           className="bg-blue-400 text-white px-6 py-3 mb-10 border-2 rounded-lg hover:bg-white hover:text-black"
-//     //           onClick={handleButtonClick} // Open the form on button click
-//     //         >
-//     //           {buttonText === "Please click button for withdraw" ? "Withdraw" : "Funds"}
-//     //         </button>
-//     //       </div>
-//     //     </div>
-//     //   </ReactCardFlip>
-
-//     //   {isFormOpen && buttonText === "Please click button for withdraw" && (
-//     //     <WithdrawForm
-//     //       isOpen={isFormOpen}
-//     //       onClose={closeForm}
-//     //       onSubmit={(amount) => {
-//     //         console.log(`Withdraw amount: ${amount}`);
-//     //         closeForm();
-//     //       }}
-//     //     />
-//     //   )}
-//     //   {isFundsPopupOpen && buttonText === "Please click button for funds" && (
-//     //     <FundsPopup isOpen={isFundsPopupOpen} onClose={closeFundsPopup} />
-//     //   )}
-//     // </div>
-//   );
-// };
+ 
 
 const Fund = () => {
+
   const [showWithdrawForm, setShowWithdrawForm] = useState(false);
-  const navigate = useNavigate(); 
+
+ 
+
   const openWithdrawForm = () => {
+
     setShowWithdrawForm(true);
+
   };
-  useEffect(()=>{
-    if(localStorage.getItem("userData")==null){
-      navigate("/loginandregister");
-    }
-    },[])
-    
 
-//   };
+ 
+
   return (
-    //     <div className="mt-12 md:mt-14 mx-10 sm:mx-14 md:mx-20 lg:mx-40 flex justify-center items-center ">
-    //   <div className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-center items-center p-10">
-    //     <FlipCard
-    //       buttonText="Please click button for withdraw"
-    //       image={image2}
-    //     />
-    //     <FlipCard
-    //       buttonText="Please click button for funds"
-    //       image={image3}
-    //     />
-    //   </div>
-    // </div>
+
     <>
+
       <div className=" flex justify-center mt-10 ">
-      <button
-          onClick={openWithdrawForm} 
+
+        <button
+
+          onClick={openWithdrawForm}
+
           className="border-2 border-[#034694] rounded-[4px] px-5 py-2 text-xl cursor-pointer"
+
         >
+
           Withdraw
+
         </button>
+
         <button className="border-2 border-[#034694] ml-5 rounded-[4px]  px-5 py-2 text-xl">
+
           Add Funds
+
         </button>
+
       </div>
-      {showWithdrawForm && <WithdrawForm isOpen={true} onClose={() => setShowWithdrawForm(false)} />}
+
+      {showWithdrawForm && (
+
+        <WithdrawForm
+
+          isOpen={true}
+
+          onClose={() => setShowWithdrawForm(false)}
+
+        />
+
+      )}
+
       <div className=" grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5  mx-5  md:mx-10 lg:mx-14  mt-5 md:mt-14   p-5">
+
         <div className="">
+
           <div className="">
+
             <img src={image2}></img>
+
           </div>
-          <div className="mt-10">
+
+          <div className="mt-10 flex justify-center ">
+
             <img src={image8}></img>
+
           </div>
-          <ul className="mt-10">
-            <li>Name: Grow capitals </li>
-            <li>Account No: 50100655630613 </li>
-            <li> IFSC code: HDFC0008694 </li>
-    
+
+          <ul className="mt-10 text-center text-lg">
+
+            <li><b>Name: </b> Grow capitals </li>
+
+            <li><b>Account No: </b> 50100655630613 </li>
+
+            <li> <b>IFSC code: </b> HDFC0008694 </li>
+
           </ul>
+
         </div>
 
+ 
+
         <div className="">
-          <div className=" md:flex md:justify-center">
-            <img   src={image5}></img>
+
+          <div className=" flex justify-center">
+
+            <img src={image5}></img>
+
           </div>
 
-          <div className="mt-10">
+ 
+
+          <div className="mt-10 ">
+
             <img src={image4}></img>
+
           </div>
+
         </div>
+
+ 
 
         <div className="">
-          <div className="md:flex md:justify-center">
+
+          <div className="flex justify-center">
+
             <img src={image3}></img>
+
           </div>
 
-          <div className="mt-10">
+ 
+
+          <div className="mt-10 flex justify-center">
+
             <img src={image7} className="  w-full md:w-4/5  h-auto"></img>
+
           </div>
-          <ul className="mt-5">
-            <li>Name: </li>
-            <li>UPI ID: capitalssgroww@okicici</li>
-            <li>Mobile Number: 1234567890</li>
-            <li>Bank Name: ICICI Bank</li>
+
+          <ul className="mt-5  text-center  text-lg ">
+
+            <li><b>UPI ID: </b> capitalssgroww@okicici</li>
+
+            <li><b>Mobile Number: </b>8962163025 </li>
+
+            <li><b>Bank Name: </b> ICICI Bank</li>
+
           </ul>
+
         </div>
+
       </div>
+
     </>
+
   );
+
 };
+
+ 
 
 export default Fund;
--- src/Modules/Account/UserDashboard.jsx
+++ src/Modules/Account/UserDashboard.jsx
@@ -15,12 +15,32 @@ const UserDashboard = () => {
   debugger
  
  }
+ const sourceDiv = document.querySelector('.tv-embed-widget-wrapper__body');
+ const targetDiv = document.getElementById('grapch');
 useEffect(()=>{
-  
+  // targetDiv?.innerHTML = sourceDiv?.innerHTML;
 if(localStorage.getItem("userData")==null){
   navigate("/loginandregister");
 }
 },[])
+const [dataValue, setDataValue] = useState([]);
+useEffect(() => {
+  var formdata = new FormData();
+  formdata.append("userEmail", localStorage.getItem("userData"));
+
+  var requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  fetch(servieUrl.url+"rolebased/UserAmountStatus/", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      setDataValue(result);
+    })
+    .catch((error) => console.log("error", error));
+}, []);
 
  useEffect(()=>{
   var formdata = new FormData();
@@ -79,62 +99,42 @@ if(localStorage.getItem("userData")==null){
 
   return (
     <>
-      <h1 className="text-2xl md:text-4xl font-bold text-center text-[#034694] mt-10">
-        Welcome To user Page
-      </h1>
+     
 
-      <h2 className="text-2xl md:text-3xl font-semibold text-center mt-5">
-        Here is the link given below
-      </h2>
-
-      <div className="text-center mt-8">
-        <Link to="/portfolio">
-          <button className="bg-[#059DC0] px-5 py-2 text-white font-semibold text-xl rounded-[4px] ml-3">
-            Portfolio
-          </button>
-        </Link>
-
-        {/* <Link to ="/addfund">
-      <button className="bg-[#059DC0] px-5 py-2 text-white font-semibold text-xl rounded-[4px] md:ml-3 mt-4 md:mt-0">
-        Buy Stock 
-      </button>
-      
-      </Link> */}
-      </div>
-
-      {/* new  */}
 
       <div>
         <div className="grid lg:grid-cols-3">
           <div className="lg:col-span-1">
             <div className="bg-white mx-5 shadow-2xl text-center">
               <img
-                src="https://bootdey.com/img/Content/avatar/avatar7.png"
+                src={base64Image ?base64Image:profile[0]?.fields?.Image?profile[0]?.fields?.Image:"https://bootdey.com/img/Content/avatar/avatar7.png"}
                 alt="Admin"
                 className="rounded-circle m-auto"
                 width="150"
               />
+             
 
               <div className="mt-3">
-                <h4>John Doe</h4>
+                <h4>{profile[0]?.fields?.first_name}</h4>
 
-                <p className="text-secondary mb-1">Full Stack Developer</p>
+                <p className="text-secondary mb-1">{profile[0]?.fields?.email}</p>
 
                 <p className="text-muted font-size-sm ">
-                  Bay Area, San Francisco, CA
+                 {profile[0]?.fields?.created_date}
                 </p>
 
-                <button className="p-2 bg-blue-600  text-white border-2 my-2">
-                  Follow
-                </button>
-
-                <button className="p-2  border-2 my-2">Message</button>
+                <label className="custom-file-input">
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+      
+      </label>
+    
+                <button className="p-2  border-2 my-2">Add Fund</button>
               </div>
             </div>
 
             <div className="flex mt-6 p-4 justify-between bg-white mx-5 shadow-2xl text-center">
               <h2 className="flex p-1">
-                <p>
+                {/* <p>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -149,16 +149,16 @@ if(localStorage.getItem("userData")==null){
                   >
                     <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                   </svg>
-                </p>{" "}
-                <p className="pl-3">Facebook</p>
+                </p>{" "} */}
+                <p className="pl-3">Balance Sheet</p>
               </h2>
 
-              <p>https://bootdey.com</p>
+              <p> {dataValue[0]?.fields?.price}</p>
             </div>
 
             <div className="flex mt-6 p-4 justify-between bg-white mx-5 shadow-2xl text-center">
               <h2 className="flex p-1">
-                <p>
+                {/* <p>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -173,16 +173,16 @@ if(localStorage.getItem("userData")==null){
                   >
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
-                </p>{" "}
-                <p className="pl-3">Github</p>
+                </p>{" "} */}
+                <p className="pl-3">Profit</p>
               </h2>
 
-              <p>bootdey</p>
+              <p> {dataValue[0]?.fields?.profit}</p>
             </div>
 
             <div className="flex mt-6 p-4 justify-between bg-white mx-5 shadow-2xl text-center">
               <h2 className="flex p-1">
-                <p>
+                {/* <p>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
@@ -197,45 +197,14 @@ if(localStorage.getItem("userData")==null){
                   >
                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                   </svg>
-                </p>{" "}
-                <p className="pl-3">Twitter</p>
+                </p>{" "} */}
+                <p className="pl-3">Loss</p>
               </h2>
 
-              <p>https://bootdey.com</p>
+              <p> {dataValue[0]?.fields?.loss}</p>
             </div>
 
-            <div className="flex mt-6 p-4 justify-between bg-white mx-5 shadow-2xl text-center">
-              <h2 className="flex p-1">
-                <p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-instagram mr-2 icon-inline text-danger"
-                  >
-                    <rect
-                      x="2"
-                      y="2"
-                      width="20"
-                      height="20"
-                      rx="5"
-                      ry="5"
-                    ></rect>
-                    <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                    <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                  </svg>
-                </p>{" "}
-                <p className="pl-3">Instagram</p>
-              </h2>
-
-              <p>https://bootdey.com</p>
-            </div>
+           
           </div>
 
           <div className="lg:col-span-2">
@@ -243,7 +212,7 @@ if(localStorage.getItem("userData")==null){
               <div className="flex  p-3 gap-5 mx-5">
                 <p className=" pr-10">FullName:</p>
 
-                <p>Kenneth Valdez</p>
+                <p>{profile[0]?.fields?.first_name}</p>
               </div>
 
               <hr />
@@ -251,7 +220,7 @@ if(localStorage.getItem("userData")==null){
               <div className="flex  p-3 gap-5 mx-5">
                 <p className="pr-10">Email:</p>
 
-                <p> fip@jukmuh.al</p>
+                <p> {profile[0]?.fields?.email}</p>
               </div>
 
               <hr />
@@ -259,18 +228,18 @@ if(localStorage.getItem("userData")==null){
               <div className="flex  p-3 gap-5 mx-5">
                 <p className="pr-10">Mobile</p>
 
-                <p> (320) 380-4539</p>
+                <p> {profile[0]?.fields?.phone_number}</p>
               </div>
 
               <hr />
 
               <div className="flex  p-3 gap-5 mx-5">
-                <p className="pr-10">Address:</p>
+                <p className="pr-10">Date Joined:</p>
 
-                <p> Bay Area, San Francisco, CA</p>
+                <p> {profile[0]?.fields?.date_joined}</p>
               </div>
               <button className="p-2  bg-blue-600 border-2 my-2 text-white">
-                Message
+                PortFolio
               </button>
               <hr />
             </div>
@@ -279,10 +248,10 @@ if(localStorage.getItem("userData")==null){
               <div className=" bg-white mx-5 p-3 py-3 shadow-2xl">
                 <div class="card-body">
                   <h6 class="d-flex align-items-center mb-3">
-                    <i class="material-icons text-info mr-2">Assignment</i>
-                    Project Status
+                    <i class="material-icons text-info mr-2">Bank Details</i>
+                   
                   </h6>
-                  <small>Web Design</small>
+                  <small>aadhaarCardNumber</small>
                   <div class="progress mb-3 ">
                     <div
                       class="progress-bar bg-primary w-[80%]"
@@ -290,9 +259,9 @@ if(localStorage.getItem("userData")==null){
                       aria-valuenow="80"
                       aria-valuemin="0"
                       aria-valuemax="100"
-                    ></div>
+                    >{profile[0]?.fields?.aadhaarCardNumber}</div>
                   </div>
-                  <small>Website Markup</small>
+                  <small>bankaccount</small>
                   <div class="progress mb-3 ">
                     <div
                       class="progress-bar bg-primary w-[72%]"
@@ -300,9 +269,9 @@ if(localStorage.getItem("userData")==null){
                       aria-valuenow="72"
                       aria-valuemin="0"
                       aria-valuemax="100"
-                    ></div>
+                    >{profile[0]?.fields?.bankaccount}</div>
                   </div>
-                  <small>One Page</small>
+                  <small>pancard</small>
                   <div class="progress mb-3 ">
                     <div
                       class="progress-bar bg-primary w-[89%]"
@@ -310,7 +279,7 @@ if(localStorage.getItem("userData")==null){
                       aria-valuenow="89"
                       aria-valuemin="0"
                       aria-valuemax="100"
-                    ></div>
+                    >{profile[0]?.fields?.pancard}</div>
                   </div>
                   <small>Mobile Template</small>
                   <div class="progress mb-3 ">
@@ -326,64 +295,9 @@ if(localStorage.getItem("userData")==null){
                 </div>
               </div>
 
-              <div className=" bg-white mx-5 p-3 py-3 shadow-2xl">
-                <div class="card-body">
-                  <h6 class="d-flex align-items-center mb-3">
-                    <i class="material-icons text-info mr-2">assignment</i>
-                    Project Status
-                  </h6>
-                  <small>Web Design</small>
-                  <div class="progress  h-10">
-                    <div
-                      class="progress-bar bg-primary w-[80%]"
-                      role="progressbar"
-                      aria-valuenow="80"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <small>Website Markup</small>
-                  <div class="progress  h-10">
-                    <div
-                      class="progress-bar bg-primary w-[72%]"
-                      role="progressbar"
-                      aria-valuenow="72"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <small>One Page</small>
-                  <div class="progress h-10">
-                    <div
-                      class="progress-bar bg-primary w-[89%]"
-                      role="progressbar"
-                      aria-valuenow="89"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <small>Mobile Template</small>
-                  <div class="progress mb-3 h-10">
-                    <div
-                      class="progress-bar bg-primary w-[55%]"
-                      role="progressbar"
-                      aria-valuenow="55"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <small>Backend API</small>
-                  <div class="progress mb-3 h-10">
-                    <div
-                      class="progress-bar bg-primary w-[66%]"
-                      role="progressbar"
-                      aria-valuenow="66"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                </div>
-              </div>
+              {/* <div className=" bg-white mx-5 p-3 py-3 shadow-2xl" id="grapch">
+              <TradingGraphc/>
+              </div> */}
             </div>
           </div>
         </div>
